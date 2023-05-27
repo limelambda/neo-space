f0d2e70d0264fd22c5d01cf94e83377b939abd9d
Ford <redalpha333@outlook.com>
2023-05-27 00:11:43 -0400

    Abstracted out some joystick stuff?

---
+++ joysticks.py
@@ -0,0 +1,25 @@
+import pygame
+
+def to_key(joys):
+    pressed = []
+    for iter, joy in enumerate(joys):  # Joysticks input by converting keys and kinda adding to pressed, defaults to player 1 controller controll
+        for button, button_pressed in {
+            button: joy.get_button(button)
+            for button in range(joy.get_numbuttons())
+        }.items():
+            if button == 3 and button_pressed:  # Fire
+                pressed.append((pygame.K_e, pygame.K_RCTRL)[iter])
+        for axis, value in {
+            axis: joy.get_axis(axis) for axis in range(joy.get_numaxes())
+        }.items():
+            if axis == 1:  # Vert
+                if value > 0.5:
+                    pressed.append((pygame.K_s, pygame.K_DOWN)[iter])
+                if value < -0.5:
+                    pressed.append((pygame.K_w, pygame.K_UP)[iter])
+            if axis == 0:  # Hor
+                if value > 0.5:
+                    pressed.append((pygame.K_d, pygame.K_RIGHT)[iter])
+                if value < -0.5:
+                    pressed.append((pygame.K_a, pygame.K_LEFT)[iter])
+    return pressed
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, socket, pickle, sys, os, random, math
+import pygame, socket, pickle, sys, os, random, math, joysticks
 
 
 def resource_path(relative_path):
@@ -258,29 +258,8 @@ def main_pt2(s=None, conn=None):
             )
             if pygame.key.get_pressed()[i]
         ]
-        if (
-            pygame.joystick.get_count() > 0
-        ):  # Joysticks input by converting keys and kinda adding to pressed, defaults to player 1 controller controll
-            for iter, joy in enumerate(joys):
-                for button, button_pressed in {
-                    button: joy.get_button(button)
-                    for button in range(joy.get_numbuttons())
-                }.items():
-                    if button == 3 and button_pressed:  # Fire
-                        pressed.append((pygame.K_e, pygame.K_RCTRL)[iter])
-                for axis, value in {
-                    axis: joy.get_axis(axis) for axis in range(joy.get_numaxes())
-                }.items():
-                    if axis == 1:  # Vert
-                        if value > 0.5:
-                            pressed.append((pygame.K_s, pygame.K_DOWN)[iter])
-                        if value < -0.5:
-                            pressed.append((pygame.K_w, pygame.K_UP)[iter])
-                    if axis == 0:  # Hor
-                        if value > 0.5:
-                            pressed.append((pygame.K_d, pygame.K_RIGHT)[iter])
-                        if value < -0.5:
-                            pressed.append((pygame.K_a, pygame.K_LEFT)[iter])
+        if pygame.joystick.get_count() > 0:
+            pressed.append(joysticks.to_key(joys))
 
         # Network connectivity lol
         if is_online:
